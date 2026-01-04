f6f2ea75f5599a741fe8b3791b996b9d39da4fb3
Carrick Bartle <carrickdb@gmail.com>
2026-01-03 18:27:36 -0800

    17.8 redo

--- 17.8-largest_rectangle_under_skyline.py
+++ 17.8-largest_rectangle_under_skyline.py
@@ -4,35 +4,41 @@ from test_framework import generic_test
 
 
 def calculate_largest_rectangle(h: List[int]) -> int:
-    s,s2 = [], []
-    l = len(h)
-    minLeft = {}
-    minRight = {}
-    for i in range(l):
-        curr = h[i]
-        while s and h[s[-1]] >= curr:
-            s.pop()
-        if s:
-            minLeft[i] = s[-1]
-        else:
-            minLeft[i] = -1
-        s.append(i)
-        j = l-i-1
-        curr = h[j]
-        while s2 and h[s2[-1]] >= curr:
-            s2.pop()
-        if s2:
-            minRight[j] = s2[-1]
-        else:
-            minRight[j] = l
-        s2.append(j)
+    s = []
     m = 0
-    for i in range(l):
-        m = max(m, h[i]*(minRight[i]-minLeft[i]-1))
+    for i,b in enumerate(h+[0]):
+        while s and h[s[-1]] >= b:
+            prevBuilding = s.pop()
+            height = h[prevBuilding]
+            width = i if not s else i-s[-1]-1
+            m = max(m, height*width)
+        s.append(i)
     return m
 
+    # This doesn't work (calculating from the last building in a series as opposed to the building after it)
+    # because it doesn't account for the rectangles going forwards from a shorter building to longer.
+    # s = [0]
+    # h = [-1] + h
+    # m = 0
+    # l = len(h)
+    # for i in range(1, l):
+    #     b = h[i]
+    #     while s and h[s[-1]] >= b:
+    #         s.pop()
+    #     width = (i - s[-1])
+    #     print(i, b, width, width*b)
+    #     m = max(m, width * b)
+    #     s.append(i)
+    # return m
 
-# print(calculate_largest_rectangle([1, 4, 2, 5, 6, 3, 2, 6, 6, 5, 2, 1, 3]))
+# print(calculate_largest_rectangle([3,2,1]))
+# print(calculate_largest_rectangle([1,2,3]))
+
+
+# test 1
+
+#0  1  2  3  4  5  6  7  8  9 10 11 12
+# [1, 4, 2, 5, 6, 3, 2, 6, 6, 5, 2, 1, 3]
 
 
 if __name__ == '__main__':
