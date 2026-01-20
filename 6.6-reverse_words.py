942af1f43b7af3b129e128e348a3afdac9023dc8
Carrick Bartle <carrickdb@gmail.com>
2026-01-19 19:20:10 -0800

    6.6

--- 6.6-reverse_words.py
+++ 6.6-reverse_words.py
@@ -7,16 +7,33 @@ from test_framework.test_utils import enable_executor_hook
 # Assume s is a list of strings, each of which is of length 1, e.g.,
 # ['r', 'a', 'm', ' ', 'i', 's', ' ', 'c', 'o', 's', 't', 'l', 'y'].
 def reverse_words(s):
-    # TODO - you fill in here.
-    return
-
+    l = len(s)
+    p1,p2 = 0,l-1
+    while p1 < p2:
+        s[p1], s[p2] = s[p2], s[p1]
+        p1 += 1
+        p2 -= 1
+    p1,p2 = 0,0
+    while True:
+        while p2 < l and s[p2] != ' ':
+            p2 += 1
+        end = p2
+        p2 -= 1
+        while p1 < p2:
+            s[p1], s[p2] = s[p2], s[p1]
+            p1 += 1
+            p2 -= 1
+        while end < l and s[end] == ' ':
+            end += 1
+        if end >= l:
+            break
+        p1, p2 = end, end
+        
 
 @enable_executor_hook
 def reverse_words_wrapper(executor, s):
     s_copy = list(s)
-
     executor.run(functools.partial(reverse_words, s_copy))
-
     return ''.join(s_copy)
 
 
