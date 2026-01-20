388aa7dec4268cb3ae226cfe236cda42f4bb0268
Carrick Bartle <carrickdb@gmail.com>
2026-01-19 18:56:02 -0800

    6.4 improvements

--- 6.4-replace_and_remove.py
+++ 6.4-replace_and_remove.py
@@ -8,19 +8,16 @@ from test_framework.test_utils import enable_executor_hook
 def replace_and_remove(size: int, s: List[str]) -> int:
     l = len(s)
     p1,p2 = 0,0
-    new_size = 0
-    while p1 < size and p2 < size:
+    while p2 < size:
         if s[p2] == "b":
             p2 += 1
             continue
         s[p1] = s[p2]
         p2 += 1
-        new_size += 1
         p1 += 1
-    final_size = sum(2 if s[i]=="a" else 1 for i in range(new_size))
-    # print(new_size)
-    p1,p2 = new_size-1, final_size-1
-    while p1 >= 0 and p2 >= 0:
+    final_size = sum(2 if s[i]=="a" else 1 for i in range(p1))
+    p1,p2 = p1-1, final_size-1
+    while p1 >= 0:
         if s[p1] == "a":
             s[p2] = "d"
             p2 -= 1
@@ -30,15 +27,6 @@ def replace_and_remove(size: int, s: List[str]) -> int:
         p2 -= 1
         p1 -= 1
     return final_size
-
-# print(replace_and_remove(6, ["b", "d", "c", "a", "b", "a"]))
-# print(replace_and_remove(4, ["a", "b", "a", "d", "b"]))
-# print(replace_and_remove(4, ["c", "b", "a", "c", "d", "b"]))
-
-# print(replace_and_remove(4, ["a", "b", "a", "d", "b"]))
-# print(replace_and_remove(2, ["b", "b"]))
-# print(replace_and_remove(1, ["b", "b"]))
-# print(replace_and_remove(4, ["b", "a", "a", "d", "c"]))
 
 @enable_executor_hook
 def replace_and_remove_wrapper(executor, size, s):
