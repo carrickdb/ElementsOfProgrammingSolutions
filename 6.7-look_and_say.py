165e82d5303491a42e90dac329012f81825d09a9
Carrick Bartle <carrickdb@gmail.com>
2026-01-24 14:57:53 -0800

    6.7

--- 6.7-look_and_say.py
+++ 6.7-look_and_say.py
@@ -1,9 +1,14 @@
 from test_framework import generic_test
-
+from itertools import groupby
 
 def look_and_say(n: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    curr = ["1"]
+    for i in range(n-1):
+        new = []
+        for val, group in groupby(curr):
+            new.extend([str(len(list(group))), val])
+        curr = new
+    return ''.join(curr)
 
 
 if __name__ == '__main__':
