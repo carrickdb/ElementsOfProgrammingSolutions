45695421d3e03ea1e247100321c025ae2c234535
Carrick Bartle <carrickdb@gmail.com>
2026-03-27 10:07:11 -0700

    9.1

--- 9.1-is_tree_balanced.py
+++ 9.1-is_tree_balanced.py
@@ -3,9 +3,18 @@ from test_framework import generic_test
 
 
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
 
+    def balanced(curr):
+        if not curr:
+            return True, 0
+        rbalanced, rh = balanced(curr.right)
+        lbalanced, lh = balanced(curr.left)
+        if not rbalanced or not lbalanced:
+            return False, 0
+        return abs(rh-lh) <= 1, max(rh, lh)+1
+
+    b, _ = balanced(tree)
+    return b
 
 if __name__ == '__main__':
     exit(
