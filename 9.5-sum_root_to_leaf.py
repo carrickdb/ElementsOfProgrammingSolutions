ec5e8a6d73bfb29a4e7f6b578785121951a0fc5b
Carrick Bartle <carrickdb@gmail.com>
2026-03-27 12:31:20 -0700

    9.5

--- 9.5-sum_root_to_leaf.py
+++ 9.5-sum_root_to_leaf.py
@@ -3,8 +3,36 @@ from test_framework import generic_test
 
 
 def sum_root_to_leaf(tree: BinaryTreeNode) -> int:
-    # TODO - you fill in here.
-    return 0
+    currNum = 0
+    ans = 0
+
+    def rec(curr):
+        if not curr:
+            return
+        nonlocal currNum
+        nonlocal ans
+        currNum <<= 1
+        currNum |= curr.data
+        # print(curr.data, currNum, ans)
+        if not curr.left and not curr.right:
+            # print("not curr.left and not curr.right")
+            ans += currNum
+            currNum >>= 1
+            return
+        # print(curr.data)
+        rec(curr.left)
+        rec(curr.right)
+        currNum >>= 1
+        # print(currNum)
+
+    rec(tree)
+    return ans
+
+
+# tree = BinaryTreeNode(1)
+# tree.left = BinaryTreeNode(0)
+# tree.right = BinaryTreeNode(1)
+# print(sum_root_to_leaf(tree))
 
 
 if __name__ == '__main__':
