9df3d2d805e78ad661ed8a770f582697e18c8b78
Carrick Bartle <carrickdb@gmail.com>
2026-05-06 21:12:19 -0700

    11.9

--- 11.9-absent_value_array.py
+++ 11.9-absent_value_array.py
@@ -3,10 +3,31 @@ from typing import Iterator
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+from itertools import tee
+from collections import Counter
 
 def find_missing_element(stream: Iterator[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    s1, s2 = tee(stream)
+    counter = Counter()
+    for ip in s1:
+        counter[ip >> 16] += 1
+    
+    for prefix, count in counter.items():
+        if count < 2**16:
+            break
+    
+    ips = set()
+    for ip in s2:
+        if (ip >> 16) != prefix:
+            continue
+        ips.add(ip & ((1<<16) - 1))
+    
+    for i in range(2**16):
+        if i not in ips:
+            return (prefix << 16) | i
+
+
+    
 
 
 def find_missing_element_wrapper(stream):
