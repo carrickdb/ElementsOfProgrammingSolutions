b276340b29f76a15b00f8edf5c42a035b8a25c2a
Carrick Bartle <carrickdb@gmail.com>
2026-05-25 18:06:24 -0700

    12.8

--- 12.8-longest_subarray_with_distinct_values.py
+++ 12.8-longest_subarray_with_distinct_values.py
@@ -2,10 +2,19 @@ from typing import List
 
 from test_framework import generic_test
 
-
+from collections import Counter
 def longest_subarray_with_distinct_entries(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    left = 0
+    longest = 0
+    counts = Counter()
+    for i, n in enumerate(A):
+        counts[n] += 1
+        while counts[n] > 1:
+            counts[A[left]] -= 1
+            left += 1
+        longest = max(longest, i-left+1)
+    return longest
+
 
 
 if __name__ == '__main__':
